eafc65c89668fcb9be5f0dd0c5b5d0429e66f101
William Liu <willgl@umich.edu>
2013-09-23 16:38:37 -0400

    test

--- src/com/example/testing123_9_15_2013/FirstActivity.java
+++ src/com/example/testing123_9_15_2013/FirstActivity.java
@@ -1,7 +1,12 @@
 package com.example.testing123_9_15_2013;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,30 +15,65 @@ import android.widget.EditText;
 
 public class FirstActivity extends Activity
 {
-
+  public ArrayList<String> undoStack = new ArrayList<String>();
+  public ArrayList<String> redoStack = new ArrayList<String>();
+  
+  EditText mEditText; 
+  
   @Override
-  protected void onCreate(Bundle savedInstanceState)
-  {
+  protected void onCreate(Bundle savedInstanceState)  {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_first);
     
+    mEditText = (EditText) findViewById(R.id.textBox);
+    
+    mEditText.addTextChangedListener(new TextWatcher() {
+      //private int time;
+      //private String oldString;
+      //Log.d ("pizza")
+      
+      public void onTextChanged(CharSequence s, int start, int before, int count) 
+      {
+        // Nothing
+      };
+      
+      @Override
+      public void afterTextChanged(Editable s)
+      {
+        Log.d("my app", s.toString());
+        //send events to server, etc.
+      }
+      
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count,
+          int after)
+      {
+        // TODO Auto-generated method stub
+        
+      }
+    });
+    
+    
     Button firstButton = (Button) findViewById(R.id.myButton);
     firstButton.setOnClickListener(new OnClickListener() {
-    
-    EditText myTextHandler = (EditText) findViewById(R.id.textBox);
         
       @Override
       public void onClick(View arg0) {
  
 //      Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.mkyong.com"));
 //      startActivity(browserIntent);
-        myTextHandler.setText("Yo bitch I changed yo text");
+        mEditText.setText("Yo bitch I changed yo text");
         
  
       }
- 
     });
+    
+    //myListeners();
   }
+  
+  //public void myListeners() {
+      
+  //}
 
 
   @Override
